99c1b2893d1f1c8bf2c909f1ed328ce06ed022b0
11AlexanderB <bezhenar93@internet.ru>
2022-09-08 12:14:36 +0300

    Intersection

---
+++ task2/Program.cs
@@ -0,0 +1,17 @@
+﻿
+void Intersection()
+{
+    Console.WriteLine("Введите данные k1: ");
+    double k1 = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите данные b1: ");
+    double b1 = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите данные k2: ");
+    double k2 = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите данные b2: ");
+    double b2 = Convert.ToInt32(Console.ReadLine());
+
+    double x = (b1-b2)/(k2-k1);
+    double y = k2*x+b2;
+    Console.WriteLine($"Координата x равна: {x};  Координата y равна: {y} ");
+}
+Intersection();
